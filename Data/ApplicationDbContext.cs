809c77d4119194f3be92645f8625a45dfb86bb2a
ilyas emre <ilyasemrekeskin@outlook.com.tr>
2023-09-19 08:53:52 +0300

    Add project files.

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using JokesWebApp.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+
+namespace JokesWebApp.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext (DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<JokesWebApp.Models.Joke> Joke { get; set; } = default!;
+
+        public DbSet<JokesWebApp.Models.User> User { get; set; } = default!;
+
+    }
+}
